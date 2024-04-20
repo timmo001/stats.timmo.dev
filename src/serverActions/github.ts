57bd06d50bec61e1138eedda26222c21edf7cb66
Aidan Timson <contact@timmo.xyz>
2024-04-20 10:30:58 +0100

    Token

--- src/serverActions/github.ts
+++ src/serverActions/github.ts
@@ -1,25 +1,22 @@
 "use server";
 import { promises as fs } from "fs";
 import { Octokit } from "octokit";
-import { createAppAuth } from "@octokit/auth-app";
 
 import { User } from "@/types/user";
 
-const octokit = new Octokit({
-  authStrategy: createAppAuth,
-  auth: {
-    appId: process.env.GITHUB_APP_ID,
-    privateKey: process.env.GITHUB_APP_PRIVATE_KEY,
-    installationId: process.env.GITHUB_APP_INSTALLATION_ID,
-  },
-});
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
 export async function getUserData(): Promise<User> {
   const query = await fs.readFile(
     `${process.cwd()}/src/graphql/user.graphql`,
     "utf-8"
   );
-  return await octokit.graphql(query, {
-    login: "timmo001",
-  });
+  try {
+    return await octokit.graphql<User>(query, {
+      login: "timmo001",
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
