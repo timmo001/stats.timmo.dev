c845880f18fe91e99390967624b6815bf669306e
Aidan Timson <aidan@timmo.dev>
2024-05-04 23:29:37 +0100

    Set padding via search params

--- src/app/github/layout.tsx
+++ src/app/github/layout.tsx
@@ -15,11 +15,15 @@ function RootLayoutChild({
   const searchParams = useSearchParams();
 
   const background = searchParams.get("background");
+  const padding = searchParams.get("padding");
 
   return (
     <body
       className={`${inter.className} bg-gradient-to-b from-indigo-950 to-slate-900 flex min-h-screen flex-col items-center justify-start p-8`}
-      style={{ background: background || undefined }}
+      style={{
+        background: background || undefined,
+        padding: padding || undefined,
+      }}
     >
       {children}
     </body>
