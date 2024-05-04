0cd316a07f24194381f4e73a9630e366c74773d0
Aidan Timson <aidan@timmo.dev>
2024-05-04 23:17:55 +0100

    Set background with query params

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,27 @@
-import type { Metadata } from "next";
+"use client";
+import { useSearchParams } from "next/navigation";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Timmo Metrics",
-  description: "Aggregated metrics for Timmo",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const searchParams = useSearchParams();
+
+  const background = searchParams.get("background");
+
   return (
     <html lang="en">
       <body
         className={`${inter.className} bg-gradient-to-b from-indigo-950 to-slate-900 flex min-h-screen flex-col items-center justify-start p-8`}
+        style={{
+          background: background || undefined,
+        }}
       >
         {children}
       </body>
