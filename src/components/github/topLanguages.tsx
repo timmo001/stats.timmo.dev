ab6699c5fa9e2688c55130fc379a01b3ddfde8b1
Aidan Timson <aidan@timmo.dev>
2024-05-05 00:07:48 +0100

    Change language badges to links

--- src/components/github/topLanguages.tsx
+++ src/components/github/topLanguages.tsx
@@ -6,14 +6,17 @@ export default function GitHubTopLanguages({
   data: Array<Language>;
 }): Array<JSX.Element> {
   return data.map((language: Language) => (
-    <span
+    <a
       className="inline-flex items-center px-3 py-1 text-sm font-medium leading-5 rounded-full"
+      href={`https://github.com/timmo001?tab=repositories&language=${language.name.toLowerCase()}`}
+      target="_blank"
+      rel="noopener noreferrer"
       style={{
         color: language.contrastColor,
         backgroundColor: language.color,
       }}
     >
       {language.name}
-    </span>
+    </a>
   ));
 }
