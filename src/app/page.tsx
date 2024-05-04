9d1048298412c4f746151788e1f3c6150927b132
Aidan Timson <aidan@timmo.dev>
2024-05-04 23:21:52 +0100

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-import { Metadata } from "next";
-
 import { USERNAME } from "@/lib/github";
 import Main from "@/components/main";
-
-export const metadata: Metadata = {
-  title: "Timmo Metrics",
-  description: "Aggregated metrics for Timmo",
-};
 
 export default async function Home() {
   return <Main username={USERNAME} />;
